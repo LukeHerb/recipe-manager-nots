0fa3c99228c072387df2cea11c986e1096d64070
Luke Herb <95945528+LukeHerb@users.noreply.github.com>
2024-11-04 16:22:41 -0500

    change app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { ref } from 'vue'
+import Todos from './components/Recipes.vue'
+import Navbar from './components/Navbar.vue'
+
+import { Authenticator } from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
+import Button from 'primevue/button'
+import { RouterView } from 'vue-router'
+
+const recipes = ref([
+  {
+    id: '1',
+    name: "Nonna's Spaghetti",
+    description:
+      'A classic spaghetti recipe passed down through generations. This recipe is sure to impress your friends and family with its rich flavors and hearty portions.',
+    createdBy: 'Luke Herb',
+    time: '30 minutes',
+    course: 'Entree',
+    numServings: 4,
+    difficulty: 'Hard',
+  },
+  {
+    id: '4',
+    name: 'Chocolate Chip Cookies',
+    description:
+      'Soft and chewy chocolate chip cookies that are perfect for any occasion.',
+    createdBy: 'Jane Doe',
+    time: '20 minutes',
+    course: 'Dessert',
+    numServings: 24,
+    difficulty: 'Easy',
+  },
+])
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <Navbar :signOut="signOut" />
+      <RouterView />
+    </template>
+  </authenticator>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  background-color: #ebe7e4;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.titles {
+  font-family: 'Playfair Display', serif;
 }
 </style>
