399e659a4d2e7074d3d97d52a8ee009b7b2b544c
Luke Herb <95945528+LukeHerb@users.noreply.github.com>
2024-11-23 21:26:04 -0500

    update image rules

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,6 +3,9 @@ import { defineStorage } from '@aws-amplify/backend'
 export const storage = defineStorage({
   name: 'recipe-manager-bucket',
   access: (allow) => ({
-    'recipe-manager/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'recipe-manager/*': [
+      allow.authenticated.to(['read', 'write', 'delete']), // Authenticated users can read, write, delete
+      allow.guest.to(['read']), // Guests can only read
+    ],
   }),
 })
