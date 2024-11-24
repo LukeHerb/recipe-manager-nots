35e34753a12a7e4857ccce80ad49ddcbb50926fe
Luke Herb <95945528+LukeHerb@users.noreply.github.com>
2024-11-24 11:51:28 -0500

    update backend for guests

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -26,7 +26,10 @@ const schema = a.schema({
       savedBy: a.string().array(),
       reviews: a.hasMany('Review', 'recipeId'), // Define relationship
     })
-    .authorization((allow) => [allow.authenticated()]),
+    .authorization((allow) => [
+      allow.guest().to(['read']), // Guest users can read.
+      allow.authenticated().to(['create', 'update', 'delete', 'read']), // Authenticated users can create, update, delete.
+    ]),
   Review: a
     .model({
       id: a.id(),
@@ -37,7 +40,10 @@ const schema = a.schema({
       createdBy: a.string(),
       owner: a.string(),
     })
-    .authorization((allow) => [allow.authenticated()]),
+    .authorization((allow) => [
+      allow.guest().to(['read']), // Guest users can read.
+      allow.authenticated().to(['create', 'update', 'delete', 'read']), // Authenticated users can create, update, delete.
+    ]),
 })
 
 export type Schema = ClientSchema<typeof schema>
